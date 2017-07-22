feee275a68149af4f758fa93d952be5f8834bc39
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-07-22 11:16:54 +0200

    Add Company, Data Layer, Data Table
    Added those new models

--- smartplan/base/models.py
+++ smartplan/base/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class DataLayer(models.Model):
+	company = models.OneToOneField(Company)
+
+	def __str__(self):
+		return self.id
+
+
+class DataTable(models.Model):
+	name = models.CharField(max_length=100)
+	dataLayer = models.ForeignKey('DataLayer')
+	creationDate = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Creation date")
+	updateDate = models.DateTimeField(auto_now=True, verbose_name="Last Update Date")
+
+	def __str__(self):
+		return self.name
